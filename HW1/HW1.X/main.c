47540d94d32c220a497fdfe62de233b07be1ab41
sameersrivastava <sameer.srivastava95@gmail.com>
2016-04-16 13:27:35 -0500

    Added code to switch between LED on and off for 0.5ms

--- HW1/HW1.X/main.c
+++ HW1/HW1.X/main.c
@@ -59,14 +59,16 @@ int main() {
     
     TRISBbits.TRISB4 = 1; //Set B4 to input button
     TRISAbits.TRISA4 = 0; //Set A4 to output for LED
-    
-    
+  
     while(1) {
-	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
-		// remember the core timer runs at half the CPU speed
+        _CP0_SET_COUNT(0);
         LATAbits.LATA4 = 1; //Turn on LED
-        
+        while(_CP0_GET_COUNT() < 12000){
+            
+        }
+        LATAbits.LATA4 = 0;
+        while(_CP0_GET_COUNT() < 24000){
+            
+        }
     }
-    
-    
 }
