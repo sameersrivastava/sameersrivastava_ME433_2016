5a780b23bce6610ff6f6583424cf97ff35ce0dfa
sameersrivastava <sameer.srivastava95@gmail.com>
2016-05-03 00:59:29 -0500

    Added setVoltage function, but need to check if this works. working on the main function.

--- HW4/HW4.X/main.c
+++ HW4/HW4.X/main.c
@@ -18,7 +18,20 @@
 
 // Initialize SPI1 Communication
 void init_spi1(){
-    
+    TRISBbits.TRISB8 = 0;
+    CS = 1;
+    SS1Rbits.SS1R = 0b0100;
+    SDI1Rbits.SDI1R = 0b0100;
+    RPB13Rbits.RPB13R = 0b0011;
+    SPI1CON = 0; // turn off the SPI1 module and reset it
+    SPI1BUF; // clear the rx buffer by reading from it
+    SPI1BRG = 0x1; // baud rate to 12 MHz [SPI4BRG = (48000000/(2*desired))-1]
+    SPI1STATbits.SPIROV = 0; // clear the overflow bit
+    SPI1CONbits.MODE32 = 0; // use 8 bit mode
+    SPI1CONbits.MODE16 = 1;
+    SPI1CONbits.CKE = 1;
+    SPI1CONbits.MSTEN = 1; // master operation  
+    SPI1CONbits.ON = 1; 
 }
 
 // send a byte via spi and return the response
@@ -32,15 +45,19 @@ unsigned char spi_io(unsigned char o) {
 
 //set Voltage function
 //
-void setVoltage(char channel, char voltage){
-    
+void setVoltage(char channel, float voltage){
+    int temp = voltage;
+    CS = 0;
+    spi_io((channel << 15) | 0x7000 | (temp << 4))
+    CS = 1;
 }
 
 int main(void) {
-  
+    init_spi1();
+    
 
-  while(1) {
-    ;
-  }
-  return 0;
+    while(1) {
+      ;
+    }
+    return 0;
 }
