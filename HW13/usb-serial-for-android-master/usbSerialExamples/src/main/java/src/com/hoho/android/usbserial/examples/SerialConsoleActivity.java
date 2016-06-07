9909754b9f1bb07a50ac18d1f34788a864bc4401
sameersrivastava <sameer.srivastava95@gmail.com>
2016-06-07 15:20:00 -0500

    Apps are almost combined.

--- HW13/usb-serial-for-android-master/usbSerialExamples/src/main/java/src/com/hoho/android/usbserial/examples/SerialConsoleActivity.java
+++ HW13/usb-serial-for-android-master/usbSerialExamples/src/main/java/src/com/hoho/android/usbserial/examples/SerialConsoleActivity.java
@@ -27,6 +27,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
@@ -50,13 +51,18 @@ import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static android.graphics.Color.blue;
+import static android.graphics.Color.green;
+import static android.graphics.Color.red;
+import static android.graphics.Color.rgb;
+
 /**
  * Monitors a single {@link UsbSerialPort} instance, showing all data
  * received.
  *
  * @author mike wakerly (opensource@hoho.com)
  */
-public class SerialConsoleActivity extends Activity {
+public class SerialConsoleActivity extends Activity implements TextureView.SurfaceTextureListener {
 
     private final String TAG = SerialConsoleActivity.class.getSimpleName();
 
@@ -149,6 +155,44 @@ public class SerialConsoleActivity extends Activity {
             }
         });
 
+        //Camera stuff
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+        mSurfaceHolder = mSurfaceView.getHolder();
+
+        mTextureView = (TextureView) findViewById(R.id.textureview);
+        mTextureView.setSurfaceTextureListener(this);
+
+        mTextView = (TextView) findViewById(R.id.cameraStatus);
+
+        paint1.setColor(0xffff0000); // red
+        paint1.setTextSize(24);
+
+        //SeekBar
+        myControl = (SeekBar) findViewById(R.id.seek1);
+        setMyControlListener();
+        //End of camera Stuff
+
+    }
+    private void setMyControlListener() {
+        myControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+                thresh = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
 
@@ -266,5 +310,108 @@ public class SerialConsoleActivity extends Activity {
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
         context.startActivity(intent);
     }
+
+    //Camera functions
+
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mCamera = Camera.open();
+        Camera.Parameters parameters = mCamera.getParameters();
+//        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        parameters.setPreviewSize(640, 480);
+        parameters.setColorEffect(Camera.Parameters.EFFECT_MONO); // black and white
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY); // no autofocusing
+        mCamera.setParameters(parameters);
+        mCamera.setDisplayOrientation(90); // rotate to portrait mode
+        parameters.setAutoWhiteBalanceLock(true);
+        try {
+            mCamera.setPreviewTexture(surface);
+            mCamera.startPreview();
+        } catch (IOException ioe) {
+            // Something bad happened
+        }
+    }
+
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        // Ignored, Camera does all the work for us
+    }
+
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        mCamera.stopPreview();
+        mCamera.release();
+        return true;
+    }
+
+    // the important function
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // Invoked every time there's a new Camera preview frame
+        mTextureView.getBitmap(bmp);
+
+        final Canvas c = mSurfaceHolder.lockCanvas();
+        if (c != null) {
+            int startY = 15;
+            int wbTotal = 0; // total mass
+            int wbCOM = 0; // total (mass time position)
+            for(int currentY = startY-5; currentY < startY+5; currentY += 1) {
+                int[] pixels = new int[bmp.getWidth()];
+                // which row in the bitmap to analyse to read
+                // only look at one row in the image
+                bmp.getPixels(pixels, 0, bmp.getWidth(), 0, currentY, bmp.getWidth(), 1); // (array name, offset inside array, stride (size of row), start x, start y, num pixels to read per row, num rows to read)
+
+                // pixels[] is the RGBA data (in black an white).
+                // instead of doing center of mass on it, decide if each pixel is dark enough to consider black or white
+                // then do a center of mass on the thresholded array
+                int[] thresholdedPixels = new int[bmp.getWidth()];
+                int[] thresholdedColors = new int[bmp.getWidth()];
+
+
+                for (int i = 0; i < bmp.getWidth(); i++) {
+                    // sum the red, green and blue, subtract from 255 to get the darkness of the pixel.
+                    // if it is greater than some value (600 here), consider it black
+                    // play with the 600 value if you are having issues reliably seeing the line
+//                    if (255*3-(red(pixels[i])+green(pixels[i])+blue(pixels[i])) > thresh*6) {
+//                    if (255*3-(red(pixels[i])+green(pixels[i])+blue(pixels[i])) > (480+thresh)) {
+                    if (255*3-(red(pixels[i])+green(pixels[i])+blue(pixels[i])) > (480+thresh)) {
+                        //Green pixels
+                        thresholdedPixels[i] = 255 * 3;
+                        thresholdedColors[i] = rgb(0, 0, 0);
+                    } else {
+                        thresholdedPixels[i] = 0;
+                        thresholdedColors[i] = rgb(0, 255, 0);
+
+                    }
+                    if(currentY == startY) {
+                        wbTotal = wbTotal + thresholdedPixels[i];
+                        wbCOM = wbCOM + thresholdedPixels[i] * i;
+                    }
+                    bmp.setPixels(thresholdedColors, 0, bmp.getWidth(), 0, currentY, bmp.getWidth(), 1);
+                }
+            }
+            int COM;
+            //watch out for divide by 0
+            if (wbTotal<=0) {
+                COM = bmp.getWidth()/2;
+            }
+            else {
+                COM = wbCOM/wbTotal;
+            }
+
+            // draw a circle where you think the COM is
+            canvas.drawCircle(COM, startY, 5, paint1);
+
+            // also write the value as text
+            canvas.drawText("COM = " + COM, 10, 200, paint1);
+//            canvas.drawText("THRESHOLD = " + thresh*6, 10, 230, paint1);
+            canvas.drawText("THRESHOLD = " + 480+thresh, 10, 230, paint1);
+            c.drawBitmap(bmp, 0, 0, null);
+            mSurfaceHolder.unlockCanvasAndPost(c);
+
+            // calculate the FPS to see how fast the code is running
+            long nowtime = System.currentTimeMillis();
+            long diff = nowtime - prevtime;
+            mTextView.setText("FPS " + 1000/diff);
+            prevtime = nowtime;
+        }
+    }
+    //End of Camera functions
 
 }
